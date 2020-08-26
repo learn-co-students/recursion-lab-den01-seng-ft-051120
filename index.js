0ab51ad726077469b1373a22cb77b8830d4eb9ca
Bryce Kennedy <btken88@gmail.com>
2020-08-26 08:53:43 -0600

    Create recursive function to print each letter in a string

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// Code your solution here!
+// Code your solution here
 
+function printString(string) {
+  console.log(string[0])
+  if (string.length > 1) {
+    const newString = string.substring(1)
+    printString(newString)
+  } else {
+    return true
+  }
+}
